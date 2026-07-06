612dd5a3a61dcccfd8063ef590c314bf2053e548
Augusto César Bisognin <augustocb@gmail.com>
2026-07-06 20:30:39 -0300

    ajuste CORS

--- storage/artifacts.tf
+++ storage/artifacts.tf
@@ -3,3 +3,15 @@ resource "aws_s3_bucket" "artifacts" {
 
   force_destroy = true
 }
+
+resource "aws_s3_bucket_cors_configuration" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  cors_rule {
+    allowed_origins = var.cors_allowed_origins
+    allowed_methods = ["GET", "HEAD", "PUT"]
+    allowed_headers = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
